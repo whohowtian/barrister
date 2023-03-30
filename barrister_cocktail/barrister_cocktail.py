4b91476041621c52a3841247386ae88dc7ce63dd
jack <jwzhijie@gmail.com>
2023-03-30 14:50:11 +0800

    Update: added dynamic rendering of fields for ingredient selection, price tabulation, added cocktail_name into cocktail db. Also added CORS. Problem with add_on db now, can't link foreign key to ingredient db for addOn_id. Creation of cocktail is working, but not the add-ons.

--- barrister_cocktail/barrister_cocktail.py
+++ barrister_cocktail/barrister_cocktail.py
@@ -2,8 +2,10 @@
 from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from sqlalchemy import func
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqlconnector://root@localhost:3306/barrister_cocktail')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -13,6 +15,7 @@ class Cocktail(db.Model):
     __tablename__ = 'cocktail'
 
     cocktail_id = db.Column(db.Integer, primary_key=True)
+    cocktail_name = db.Column(db.String(255), primary_key=True)
     first_spirit = db.Column(db.Integer, nullable=False)
     second_spirit = db.Column(db.Integer, nullable=False)
     sweet_base = db.Column(db.Integer, nullable=False)
@@ -21,8 +24,9 @@ class Cocktail(db.Model):
     cocktail_price = db.Column(db.Float(precision=2), nullable=False)
 
 
-    def __init__(self, cocktail_id, first_spirit, second_spirit, sweet_base, sour_base, user_id, cocktail_price):
+    def __init__(self, cocktail_id, cocktail_name, first_spirit, second_spirit, sweet_base, sour_base, user_id, cocktail_price):
         self.cocktail_id = cocktail_id
+        self.cocktail_name = cocktail_name
         self.first_spirit = first_spirit
         self.second_spirit = second_spirit
         self.sweet_base = sweet_base
@@ -31,7 +35,7 @@ class Cocktail(db.Model):
         self.cocktail_price = cocktail_price
 
     def json(self):
-        return {"cocktail_id": self.cocktail_id, "first_spirit": self.first_spirit, "second_spirit": self.second_spirit, 
+        return {"cocktail_id": self.cocktail_id, "cocktail_name": self.cocktail_name, "first_spirit": self.first_spirit, "second_spirit": self.second_spirit, 
                 "sweet_base": self.sweet_base, "sour_base": self.sour_base, "user_id": self.user_id, "cocktail_price": self.cocktail_price}
 
 class Add_ons(db.Model):
@@ -92,9 +96,9 @@ def get_all_cocktail():
 
 #Find cocktail using cocktail_id
 @app.route("/barrister_cocktail/cocktail/<string:cocktail_id>")
-def find_by_cocktail_id(cocktail_id):
+def find_cocktail_by_id(cocktail_id):
     cocktail = Cocktail.query.filter_by(cocktail_id=cocktail_id).first()
-    if book:
+    if cocktail:
         return jsonify(
             {
                 "code": 200,
@@ -110,7 +114,7 @@ def find_by_cocktail_id(cocktail_id):
 
 #Get addons list in one cocktail recipe using cocktail_id
 @app.route("/barrister_cocktail/addons/<string:cocktail_id>")
-def find_by_cocktail_id(cocktail_id):
+def find_addons_by_cocktail_id(cocktail_id):
     addonlist = Add_ons.query.filter_by(cocktail_id=cocktail_id)
     if addonlist:
         return jsonify(
@@ -124,34 +128,54 @@ def find_by_cocktail_id(cocktail_id):
     return jsonify(
         {
             "code": 404,
-            "message": "Book not found."
+            "message": "No addons found."
         }
     ), 404
 
-#Create new cocktail recipe (NOT TESTED YET)
+#Create new cocktail recipe
 @app.route("/barrister_cocktail/create_cocktail", methods=['POST'])
 def create_cocktail():
-    data = request.get_json()
-    cocktail = Cocktail(data)
+    cocktail = request.get_json()
+    cocktailName = cocktail['name']
+
+    if (Cocktail.query.filter_by(cocktail_name=cocktailName).first()):
+        return jsonify(
+            {
+                "message": "Cocktail already exists."
+            }
+        ), 400
+
+    max_cocktail_id = db.session.query(func.max(Cocktail.cocktail_id)).first()
+
+    if max_cocktail_id[0] == None:
+        cocktailID = 1
+    else:
+        cocktailID = max_cocktail_id[0] + 1
+
+    newCocktail = Cocktail(cocktailID, cocktailName, cocktail['spirit1'], cocktail['spirit2'], 
+                        cocktail['sweet'], cocktail['sour'], cocktail['user'], cocktail['total'])
 
     try:
-        db.session.add(cocktail)
+        db.session.add(newCocktail)
         db.session.commit()
+
+        for addOn in cocktail['add_ons']:
+            db.session.add(Add_ons(addOn, cocktailID))
+            db.session.commit()
+
     except:
         return jsonify(
             {
-                "code": 500,
-                "message": "An error occurred creating the book."
+                "message": "An error occurred creating the cocktail."
             }
         ), 500
-
-
+ 
     return jsonify(
         {
-            "code": 201,
-            "data": cocktail.json()
+            "data": newCocktail.json()
         }
     ), 201
+
 
 if __name__ == '__main__':
     app.run(port=3003, debug=True)
--- barrister_ingredient/barrister_ingredient.py
+++ barrister_ingredient/barrister_ingredient.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqlconnector://root@localhost:3306/barrister_ingredient')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
